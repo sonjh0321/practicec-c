59d7dcc0da4dcbd8e8dc41a264d2182a27d29067
sonjh0321 <sonjh162@gmail.com>
2021-11-02 21:53:55 +0900

    10진정수를 8,16정수로 출력하고 문자아스키코드 출력하기

--- practicec/practicec.cpp
+++ practicec/practicec.cpp
@@ -3,12 +3,14 @@
 int main()
 {
 	char a = 'A';
-	char b[5] = "ABCD";
-	int c = 100;
-	double d = 123.456;
+	char b = 'a';
+	int c = 10;
+	
+	printf("대문자 A의 아스키코드값 %d\n", a);
+	printf("소문자 a의 아스키코드값 %d\n", b);
+	printf("10진수 %d을 8진수로 표현하면 %o\n", c,c);
+	printf("10진수 %d을 10진수로 표현하면 %x\n", c, c);
+	printf("10진수 %d을 16진수로 표현하면 %X\n", c,c);
 
-	printf("%c %s \n", a, b); //%c라는 부분에 a가 들어가고 %s에 b가 들어간다
-	printf("%d %f \n",c, d);
-	printf("%e %E \n", d, d);
 	return 0;
 }
