a94e94b4c425582aa9b8f53098f3bf5fa144e0c1
sonjh0321 <sonjh162@gmail.com>
2021-11-02 21:48:21 +0900

    출력서식을 이용해 출력하기

--- practicec/practicec.cpp
+++ practicec/practicec.cpp
@@ -1,6 +1,14 @@
 ﻿#include <stdio.h>
+
 int main()
 {
-	printf("rgm-79");
+	char a = 'A';
+	char b[5] = "ABCD";
+	int c = 100;
+	double d = 123.456;
+
+	printf("%c %s \n", a, b); //%c라는 부분에 a가 들어가고 %s에 b가 들어간다
+	printf("%d %f \n",c, d);
+	printf("%e %E \n", d, d);
 	return 0;
 }
