613d68b303ec9ec88b09cbe5e38a80e84e15437d
sonjh0321 <sonjh162@gmail.com>
2021-11-02 21:58:27 +0900

    자리수를 지정하여 출력하기

--- practicec/practicec.cpp
+++ practicec/practicec.cpp
@@ -3,14 +3,14 @@
 int main()
 {
 	char a = 'A';
-	char b = 'a';
-	int c = 10;
-	
-	printf("대문자 A의 아스키코드값 %d\n", a);
-	printf("소문자 a의 아스키코드값 %d\n", b);
-	printf("10진수 %d을 8진수로 표현하면 %o\n", c,c);
-	printf("10진수 %d을 10진수로 표현하면 %x\n", c, c);
-	printf("10진수 %d을 16진수로 표현하면 %X\n", c,c);
+	char b[5] = "ABCD";
+	int c = 100;
+	double d = 123.4567;
+
+	printf("%5c %10s\n", a,b);
+	printf("%-5c %-10s\n", a, b);
+	printf("%5d %-5d\n", c, c);
+	printf("%0.3f\n", d);
 
 	return 0;
 }
