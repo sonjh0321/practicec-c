7ee8ad363f14e268085d315e819dc5efa3ee95f9
sonjh0321 <sonjh162@gmail.com>
2021-11-06 17:45:53 +0900

    function

--- practicec/practicec.cpp
+++ practicec/practicec.cpp
@@ -1,13 +1,17 @@
 ﻿#include <stdio.h>
+void s_to_b(char c[])
+{
+	int cnt = -1;
+	while (c[++cnt])
+	{
+		if (c[cnt] >= 'a' && c[cnt] <= 'z') c[cnt] -= 32;
+	}
+}
 int main()
 {
-	char week[7][10] = {"sunday","monday", "tuesday", "wednesday", "thursday",
-		"friday", "saturday"};
-	int i,j;
-	for (i = 0; i < 7; ++i)
-	{
-		for (j = 0; j < 10; ++j)printf("%c", week[i][j]);
-		printf("\n");
-	}
-		return 0;
+	char blog[5] = { "blog" };
+	printf("%s\n",blog);
+	s_to_b(blog);
+	printf("%s\n",blog);
+	return 0;
 }
