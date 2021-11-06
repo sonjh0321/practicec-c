73c8efd863b6de871f7975ae5c96da04fe113a8f
sonjh0321 <sonjh162@gmail.com>
2021-11-06 10:54:07 +0900

    switch case

--- practicec/practicec.cpp
+++ practicec/practicec.cpp
@@ -1,14 +1,22 @@
 ﻿#include <stdio.h>
-
 int main()
 {
 	int gubum;
-	printf("주민번호첫번째 숫자를 입력하세요");
+
+	printf("숫자를 입력하세요");
 	scanf("%d", &gubum);
-
-	if (1 == gubum || 3 == gubum) printf("남자입니다\n");
-	else if (2==gubum || 4==gubum) printf("여자입니다\n");
-	else printf("누구세요?\n");
-
+	switch (gubum)
+	{
+		case 1:
+		case 3:
+			printf("남자입니다\n");
+			break;
+		case 2:
+		case 4:
+			printf("여자입니다\n");
+			break;
+		default:
+			printf("없는 유형입니다\n");
+	}
 	return 0;
 }
