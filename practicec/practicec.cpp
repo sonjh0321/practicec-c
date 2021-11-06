45be0249c52511b9e27a7c78395b432d26a5c1a2
sonjh0321 <sonjh162@gmail.com>
2021-11-06 13:13:24 +0900

    for

--- practicec/practicec.cpp
+++ practicec/practicec.cpp
@@ -1,22 +1,14 @@
 ﻿#include <stdio.h>
 int main()
 {
-	int gubum;
-
-	printf("숫자를 입력하세요");
-	scanf("%d", &gubum);
-	switch (gubum)
+	int i, j;
+	for (i = 1; i <= 5; ++i)
 	{
-		case 1:
-		case 3:
-			printf("남자입니다\n");
-			break;
-		case 2:
-		case 4:
-			printf("여자입니다\n");
-			break;
-		default:
-			printf("없는 유형입니다\n");
+		for (j = 1; j < i; ++j)
+		{
+			printf(" ");
+		}
+		printf("%d\n",i);
 	}
 	return 0;
 }
