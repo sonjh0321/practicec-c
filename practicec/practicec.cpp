e5824f1a21c5a50c5d191df5e0a534b370cce12d
sonjh0321 <sonjh162@gmail.com>
2021-11-06 19:57:43 +0900

    math.h

--- practicec/practicec.cpp
+++ practicec/practicec.cpp
@@ -1,17 +1,12 @@
 ﻿#include <stdio.h>
-#include <ctype.h>
+#include <math.h>
 int main()
 {
-	char exp[7] = { "Gundam" };
-	int i;
+	double x = 2.0, y = 3.0, z = -4.5;
 
-	printf("변경 전 %s\n",exp);
-	for (i = 0; i < 6; ++i)
-	{
-		if (islower(exp[i])) exp[i] = toupper(exp[i]);
-		else if (isupper(exp[i])) exp[i] = tolower(exp[i]);
-	}
-	printf("변경 후 %s",exp);
+	printf("%d의 %d제곱은 %d이다\n",(int)x,(int)y,(int)pow(x,y));
+	printf("%d의 제곱근은 %0.1lf\n",(int)y,sqrt(x));
+	printf("%0.1lf의 절대값은 %0.1lf\n",z,fabs(z));
 
 	return 0;
 }
