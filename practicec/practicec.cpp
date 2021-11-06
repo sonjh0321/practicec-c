9bfe4685a854301914dd2feca1fdc22a37b6ac62
sonjh0321 <sonjh162@gmail.com>
2021-11-06 19:16:55 +0900

    문자열처리함수

--- practicec/practicec.cpp
+++ practicec/practicec.cpp
@@ -1,17 +1,15 @@
 ﻿#include <stdio.h>
-void s_to_b(char c[])
-{
-	int cnt = -1;
-	while (c[++cnt])
-	{
-		if (c[cnt] >= 'a' && c[cnt] <= 'z') c[cnt] -= 32;
-	}
-}
+#include <string.h>
 int main()
 {
-	char blog[5] = { "blog" };
-	printf("%s\n",blog);
-	s_to_b(blog);
-	printf("%s\n",blog);
+	char A[] = "gundam rx79";
+	char* p;
+
+	p = strchr(A,'x');
+	if (p != NULL) printf("%s\n",p);
+
+	p = strstr(A, "rx79");
+	if (p != NULL) printf("%s\n",p);
+
 	return 0;
 }
