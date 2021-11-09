fb9f37d1858eb2cb784880c650dde2d01c57d82a
sonjh0321 <sonjh162@gmail.com>
2021-11-09 18:51:36 +0900

    stdlib.h

--- practicec/practicec.cpp
+++ practicec/practicec.cpp
@@ -1,12 +1,13 @@
 ﻿#include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 int main()
 {
-	double x = 2.0, y = 3.0, z = -4.5;
-
-	printf("%d의 %d제곱은 %d이다\n",(int)x,(int)y,(int)pow(x,y));
-	printf("%d의 제곱근은 %0.1lf\n",(int)y,sqrt(x));
-	printf("%0.1lf의 절대값은 %0.1lf\n",z,fabs(z));
+	system("cls");
+	system("date");
+	system("time");
+	system("ver");
+	exit(0);
+	system("cls");
 
 	return 0;
 }
