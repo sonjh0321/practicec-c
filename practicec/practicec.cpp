3c65324d5a0acb9ddf695e81c7c451ab17fc2c73
sonjh0321 <sonjh162@gmail.com>
2021-11-15 15:51:40 +0900

    비트관련연산자

--- practicec/practicec.cpp
+++ practicec/practicec.cpp
@@ -1,20 +1,18 @@
 ﻿#include <stdio.h>
-struct mydb
-{
-	char flower[20];
-	char color[20];
-	int number;
-}ilike;
 int main()
-{
-	char a, b[3], c[] = "rx75", *d;
-	int m, n[3], *p;
-	double x, y[3], *z;
-
-	printf("char %d %d %d\n",sizeof(a),sizeof(b),sizeof(c));
-	printf("int %d %d\n", sizeof(m), sizeof(n));
-	printf("double %d %d\n", sizeof(x), sizeof(y));
-	printf("pointer %d %d %d\n", sizeof(d), sizeof(p), sizeof(z));
-	printf("struct %d\n", sizeof(ilike));
+{	
+	int i, n = 1;
+	for (i = 1; i <= 5; ++i)
+	{
+		printf("%d ", n);
+		n = n << 1;
+	}
+	printf("\n");
+	for (i = 5; i >= 1; --i)
+	{
+		n = n >> 1;
+		printf("%d ", n);
+	}
+	printf("\n");
 	return 0;
 }
